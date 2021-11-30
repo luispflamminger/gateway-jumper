12736958205603d5fdc8e671aaa660248827aace
BilekT <tomas.bilek@t-mobile.cz>
2021-11-30 13:34:48 +0100

    httpClient with explicit DT ciphers

--- src/main/java/jumper/Application.java
+++ src/main/java/jumper/Application.java
@@ -191,12 +191,43 @@ public class Application {
             SslContext s = SslContextBuilder
                     .forClient()
                     .trustManager(InsecureTrustManagerFactory.INSTANCE)
+                    //missing
                     //.ciphers(List.of("TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384"))
-                    .ciphers(List.of("TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384", "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384", "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256", "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256", "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384", "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA", "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA", "TLS_RSA_WITH_AES_128_GCM_SHA256", "TLS_RSA_WITH_AES_128_CBC_SHA", "TLS_RSA_WITH_AES_256_CBC_SHA", "TLS_AES_128_GCM_SHA256", "TLS_AES_256_GCM_SHA384"))
+                    //current list + missing
+                    //.ciphers(List.of("TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384", "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384", "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256", "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256", "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384", "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA", "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA", "TLS_RSA_WITH_AES_128_GCM_SHA256", "TLS_RSA_WITH_AES_128_CBC_SHA", "TLS_RSA_WITH_AES_256_CBC_SHA", "TLS_AES_128_GCM_SHA256", "TLS_AES_256_GCM_SHA384"))
+                    //DT exact list
+                    .protocols("TLSv1.2","TLSv1.3")
+                    .ciphers(List.of("TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384"
+                            ,"TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384"
+                            ,"TLS_DHE_DSS_WITH_AES_256_GCM_SHA384"
+                            ,"TLS_DHE_RSA_WITH_AES_256_GCM_SHA384"
+                            ,"TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256"
+                            ,"TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256"
+                            ,"TLS_DHE_RSA_WITH_CHACHA20_POLY1305_SHA256"
+                            //,"TLS_ECDHE_ECDSA_WITH_AES_256_CCM"
+                            //,"TLS_DHE_RSA_WITH_AES_256_CCM"
+                            ,"TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256"
+                            ,"TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256"
+                            ,"TLS_DHE_DSS_WITH_AES_128_GCM_SHA256"
+                            ,"TLS_DHE_RSA_WITH_AES_128_GCM_SHA256"
+                            //,"TLS_ECDHE_ECDSA_WITH_AES_128_CCM"
+                            //,"TLS_DHE_RSA_WITH_AES_128_CCM"
+                            ,"TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384"
+                            ,"TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384"
+                            ,"TLS_DHE_DSS_WITH_AES_256_CBC_SHA256"
+                            ,"TLS_DHE_RSA_WITH_AES_256_CBC_SHA256"
+                            ,"TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256"
+                            ,"TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256"
+                            ,"TLS_DHE_DSS_WITH_AES_128_CBC_SHA256"
+                            ,"TLS_DHE_RSA_WITH_AES_128_CBC_SHA256"
+                            ,"TLS_AES_256_GCM_SHA384"
+                            ,"TLS_CHACHA20_POLY1305_SHA256"
+                            ,"TLS_AES_128_GCM_SHA256"
+                            //,"TLS_AES_128_CCM_SHA256"
+                     ))
                     .build();
 
             return httpClient -> {
-                //todo rather get existing and add new, but HttpClientSecure is not public
                 //List l = ((HttpClientSecure) httpClient).sslProvider.sslContext.cipherSuites();
                 return httpClient
                         .secure(t -> t.sslContext(s));
