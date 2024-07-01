98de0fbbc8a28e526b333c23dcb4cbd84f864fbc
BilekT <tomas.bilek@t-mobile.cz>
2024-07-01 09:56:06 +0200

    fix: pass spectre info as header

--- src/main/java/jumper/Constants.java
+++ src/main/java/jumper/Constants.java
@@ -54,6 +54,10 @@ public class Constants {
   public static final String HEADER_X_FORWARDED_PORT_PORT = "443";
   public static final String HEADER_X_FORWARDED_PROTO_HTTPS = "https";
 
+  public static final String HEADER_X_SPECTRE_ISSUE = "x-spectre-issue";
+  public static final String HEADER_X_SPECTRE_PROVIDER = "x-spectre-provider";
+  public static final String HEADER_X_SPECTRE_CONSUMER = "x-spectre-consumer";
+
   public static final String HEADER_X_FAILOVER_SKIP_ZONE = "x-failover-skip-zone";
 
   public static final String QUERY_PARAM_LISTENER = "listener";
